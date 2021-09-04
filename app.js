5fdd12a6fb9b13bf125a0dcedc9b803eec608cc1
i_rei <kahow46@gmail.com>
2021-09-04 15:32:09 +0900

    make two cards with random

--- app.js
+++ app.js
@@ -2,26 +2,46 @@ phina.globalize();
 
 const ASSETS = {
   image: {
-    'heart01' : 'images/heart01.png',
-    'heart02' : 'images/heart02.png',
-    'heart03' : 'images/heart03.png',
-    'heart04' : 'images/heart04.png',
-    'heart05' : 'images/heart05.png',
-    'heart06' : 'images/heart06.png',
-    'heart07' : 'images/heart07.png',
-    'heart08' : 'images/heart08.png',
-    'heart09' : 'images/heart09.png',
-    'heart10' : 'images/heart10.png',
+    '1' : 'images/heart01.png',
+    '2' : 'images/heart02.png',
+    '3' : 'images/heart03.png',
+    '4' : 'images/heart04.png',
+    '5' : 'images/heart05.png',
+    '6' : 'images/heart06.png',
+    '7' : 'images/heart07.png',
+    '8' : 'images/heart08.png',
+    '9' : 'images/heart09.png',
+    '10' : 'images/heart10.png',
   },
 };
+
+let cardNum1 = 0;
+let cardNum2 = 0;
+let cardNum3 = 0;
+let cardNum4 = 0;
 
 phina.define("MainScene", {
   superClass: "DisplayScene",
   init() {
     this.superInit();
-    this.backgroundColor = "white";
+    this.backgroundColor = "skyblue";
 
-    Sprite('heart01').addChildTo(this).setPosition(320, 480);
+    //1から10の乱数を取得
+    let randnum = 1 + Math.floor(Math.random() * 10);
+
+    const card1 = Sprite(randnum.toString())
+      .addChildTo(this)
+      .setPosition(this.gridX.span(4), this.gridY.span(8))
+      .setScale(0.5,0.5);
+    cardNum1 = randnum;
+
+    randnum = 1 + Math.floor(Math.random() * 10);
+
+    const card2 = Sprite(randnum.toString())
+      .addChildTo(this)
+      .setPosition(this.gridX.span(12), this.gridY.span(8))
+      .setScale(0.5,0.5);
+    cardNum2 = randnum;
   },
   update() {
   },
@@ -30,7 +50,7 @@ phina.define("MainScene", {
 
 phina.main(() => {
   const app = GameApp({
-    startLabel: "main",
+    startLabel: "title",
     assets: ASSETS,
   });
   app.run();
