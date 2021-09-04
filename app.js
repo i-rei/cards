8486bc96a0e13d5f17372666d7c7fa4abf227d72
i_rei <kahow46@gmail.com>
2021-09-04 14:55:05 +0900

    add images

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 phina.globalize();
 
+const ASSETS = {
+  image: {
+    'heart01' : 'images/heart01.png',
+    'heart02' : 'images/heart02.png',
+    'heart03' : 'images/heart03.png',
+    'heart04' : 'images/heart04.png',
+    'heart05' : 'images/heart05.png',
+    'heart06' : 'images/heart06.png',
+    'heart07' : 'images/heart07.png',
+    'heart08' : 'images/heart08.png',
+    'heart09' : 'images/heart09.png',
+    'heart10' : 'images/heart10.png',
+  },
+};
+
 phina.define("MainScene", {
   superClass: "DisplayScene",
   init() {
     this.superInit();
     this.backgroundColor = "white";
+
+    Sprite('heart01').addChildTo(this).setPosition(320, 480);
   },
   update() {
   },
@@ -14,6 +31,7 @@ phina.define("MainScene", {
 phina.main(() => {
   const app = GameApp({
     startLabel: "main",
+    assets: ASSETS,
   });
   app.run();
 });
